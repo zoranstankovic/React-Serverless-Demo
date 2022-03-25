0c02d6bfa959b0793a0f576184dc363a50e76c51
Zoran <6569042+zoranstankovic@users.noreply.github.com>
2022-03-25 10:46:04 +0100

    Implement timer
    Implement timer for the game.

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import {
   StyledCharacter,
   StyledGame,
@@ -7,14 +9,49 @@ import {
 import { Strong } from "../styled/Random";
 
 export default function Game() {
+  const navigate = useNavigate();
+  const [score, setScore] = useState(0);
+  const MAX_SECONDS = 5;
+  const [ms, setMs] = useState(0);
+  const [seconds, setSeconds] = useState(MAX_SECONDS);
+
+  useEffect(() => {
+    const currentTime = new Date();
+    const interval = setInterval(() => updateTime(currentTime, 1));
+    return () => clearInterval(interval);
+  }, []);
+
+  const updateTime = (startTime) => {
+    const endTime = new Date();
+    const msPassedStr = (endTime.getTime() - startTime.getTime()).toString();
+    const formattedMSString = ("0000" + msPassedStr).slice(-5);
+    const updatedSeconds =
+      MAX_SECONDS - parseInt(formattedMSString.substring(0, 2)) - 1;
+    const updatedMs =
+      1000 -
+      parseInt(formattedMSString.substring(formattedMSString.length - 3));
+
+    setSeconds(updatedSeconds.toString().padStart(2, "0"));
+    setMs(updatedMs.toString().padStart(3, "0"));
+  };
+
+  useEffect(() => {
+    if (seconds <= -1) {
+      navigate("/gameOver");
+    }
+  }, [seconds, navigate]);
+
   return (
     <StyledGame>
       <StyledScore>
-        Score: <Strong>0</Strong>
+        Score: <Strong>{score}</Strong>
       </StyledScore>
       <StyledCharacter>A</StyledCharacter>
       <StyledTimer>
-        Time: <Strong>00: 0000</Strong>
+        Time:{" "}
+        <Strong>
+          {seconds}: {ms}
+        </Strong>
       </StyledTimer>
     </StyledGame>
   );

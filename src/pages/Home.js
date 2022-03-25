ce1d019578e3a063b3fbbff2c8c7c068ff8eb116
Zoran <6569042+zoranstankovic@users.noreply.github.com>
2022-03-25 07:56:13 +0100

    Add style to home page
    Add style to home page and fix typos regarding css navbar.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,13 @@
+import CTA from "../styled/CTA";
+import { Accent, StyledTitle } from "../styled/Random";
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <StyledTitle>Ready to type?</StyledTitle>
+      <CTA to="/game">
+        Click or type <Accent>'s'</Accent> to start playing!
+      </CTA>
+    </div>
+  );
 }
--- src/styled/Navbar.js
+++ src/styled/Navbar.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 
 export const StyledNavbar = styled.nav`
   display: grid;
-  grid-template-columns: 1rf auto;
+  grid-template-columns: 1fr auto;
   padding: 20px;
 `;
 

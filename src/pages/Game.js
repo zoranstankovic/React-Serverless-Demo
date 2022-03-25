880f589fc41f2ed740d53c9a1f06b328750e8d32
Zoran <6569042+zoranstankovic@users.noreply.github.com>
2022-03-25 08:33:12 +0100

    Style the game page
    Style the game page

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,3 +1,21 @@
+import {
+  StyledCharacter,
+  StyledGame,
+  StyledScore,
+  StyledTimer,
+} from "../styled/Game";
+import { Strong } from "../styled/Random";
+
 export default function Game() {
-  return <div>Game</div>;
+  return (
+    <StyledGame>
+      <StyledScore>
+        Score: <Strong>0</Strong>
+      </StyledScore>
+      <StyledCharacter>A</StyledCharacter>
+      <StyledTimer>
+        Time: <Strong>00: 0000</Strong>
+      </StyledTimer>
+    </StyledGame>
+  );
 }
